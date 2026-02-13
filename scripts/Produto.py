92503b58739dc19c44e7f00b628ef80107a3ab61
PedroHON410 <pedro.hon200511@gmail.com>
2026-02-13 10:53:06 -0300

    Modificando a classe produto e vendas

--- scripts/Produto.py
+++ scripts/Produto.py
@@ -112,37 +112,3 @@ class Produto:
     #         return 'Categoria já existe'
     #     else:
     #         return 'Erro inesperado'
-
-class Cliente:
-    
-    def __init__(self, id_cliente, nome_cliente, CPF, pontos):
-        self.id_cliente = id_cliente
-        self.nome_cliente = nome_cliente
-        self.historico_compras = []
-        self.CPF = CPF
-        self.pontos = pontos
-        self.cliente_novo = {}
-        self.clientes_cadastrados = []
-
-    def novo_cliente (self):
-        if not self.clientes_cadastrados:
-            novo_id = 1
-        else:
-            ultimo_cliente = self.clientes_cadastrados[-1]
-            novo_id = ultimo_cliente['id_cliente'] + 1
-        self.id_cliente = novo_id
-        self.nome_cliente = str(input('Nome do cliente: '))
-        self.CPF = str(input('CPF do cliente: '))
-        self.cliente_novo = {
-            'id_cliente': self.id_cliente,
-            'nome_cliente': self.nome_cliente,
-            'CPF': self.CPF,
-            'pontos': self.pontos
-        }
-        return self.clientes_cadastrados.append(self.cliente_novo)
-
-if __name__ == "__main__":
-    produto = Produto(1, 'Caneta', 2.5, 100)
-    produto.nova_categoria()
-    produto.novo_produto()
-    print(produto.list_categoria)
--- scripts/Venda.py
+++ scripts/Venda.py
@@ -1,17 +1,45 @@
+from db_gerenciamento import create_connection, close_connection
+
 class Venda:
-    def buscar_produto_venda(self):
-        self.id_produto_venda = id_produto_venda
-        if self.id_produto_venda in Produto.produtos_cadastrados:
-            return Produto.produtos_cadastrados[self.id_produto_venda]
-        else:
-            return 'Produto não encontrado para venda'
-        
-    def __init__(self, id_venda, qtd_venda, desconto):
+    def __init__(self, id_venda, produto_venda, qtd_venda, desconto, id_produto_venda=None):
         self.id_venda = id_venda
+        self.produto_venda = produto_venda
         self.qtd_venda = qtd_venda
         self.desconto = desconto
+        self.id_produto_venda = id_produto_venda
+
+    @staticmethod
+    #Aqui deve ser implementada a função de buscar produto para venda, que deve solicitar o nome do produto ao usuário e buscar o produto no banco de dados. 
+    # Caso o produto seja encontrado, deve retornar o id do produto para ser utilizado na venda.
+    def buscar_produto_venda(self, produto_venda):
+        connection = create_connection()
+        cursor = connection.cursor()
+        try:
+            select_query = """ SELECT id_produto FROM produtos WHERE nome = %s"""
+            cursor.execute(select_query, (produto_venda,))
+            result = cursor.fetchone()
+            if result:
+                self.id_produto_venda = result[0]
+                return self.id_produto_venda
+            else:
+                return 'Produto não encontrado para venda'
+        except Exception as e:
+            print(f"Erro ao buscar produto para venda: {e}")
 
 
-    def vender (self): 
-        self.id_venda
+    @staticmethod
+    def vender(self, id_produto_venda, qtd_venda, desconto):
+        connection = create_connection()
+        cursor = connection.cursor()
+        try:
+            insert_query = """ INSERT INTO vendas (id_produto_venda, qtd_venda, desconto) VALUES (%s, %s, %s)"""
+            values = (id_produto_venda, qtd_venda, desconto)
+            cursor.execute(insert_query, values)
+            connection.commit()
+            print("Venda realizada com sucesso")
+        except Exception as e:
+            print(f"Erro ao realizar venda: {e}")
+        finally:
+            close_connection(connection)
+        
         
