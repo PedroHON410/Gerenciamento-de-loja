f7d2efb9b89eb0800572a95916bef30efa0faac8
PedroHON410 <pedro.hon200511@gmail.com>
2026-02-15 15:13:19 -0300

    Finalizando a classe venda e atualizando o GerenciamentoEstoque

--- scripts/Venda.py
+++ scripts/Venda.py
@@ -11,30 +11,33 @@ class Venda:
         connection = create_connection()
         cursor = connection.cursor()
         try:
-            # 1. Busca o produto para pegar ID e Preço atual
-            cursor.execute("SELECT id_produto, preco FROM produtos WHERE nome = %s", (self.produto_nome,))
+            #Busca o produto para pegar ID e Preço atual
+            select_query = """ SELECT id, preco FROM produtos WHERE nome = %s"""
+            cursor.execute(select_query, (self.produto_nome,))
             produto = cursor.fetchone()
 
             if not produto:
                 print("Produto não encontrado!")
                 return
-
             self.id_produto = produto[0]
-            self.valor_unitario = produto[2]
-            
-            # 2. Realiza o cálculo
+            self.valor_unitario = produto[1]
+            #Aqui resgata o ID do produto e o valor unitário para realizar o cálculo da venda e atualizar o estoque.            
+            #Realiza o cálculo
             if self.desconto > 0:
                 valor_total = (self.valor_unitario * self.qtd_venda) - (self.desconto/100)
             else:
                 valor_total = self.valor_unitario * self.qtd_venda
 
-            # 3. Insere na tabela de vendas (incluindo o ID do produto e valor total)
+            #Insere na tabela de vendas (incluindo o ID do produto e valor total)
             insert_query = """ 
                 INSERT INTO vendas (id_produto, qtd_venda, desconto, valor_total) 
                 VALUES (%s, %s, %s, %s) 
             """
             cursor.execute(insert_query, (self.id_produto, self.qtd_venda, self.desconto, valor_total))
-            
+
+            #Atualiza o estoque do produto
+            update_query = "UPDATE produtos SET qtd_estoque = qtd_estoque - %s WHERE id_produto = %s"
+            cursor.execute(update_query, (self.qtd_venda, self.id_produto))
             connection.commit()
             print(f"Venda de {self.produto_nome} realizada! Total: R$ {valor_total:.2f}")
 
