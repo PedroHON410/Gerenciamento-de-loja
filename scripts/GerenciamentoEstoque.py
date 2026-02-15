c1995007f4943d2c6995b5dec8ed7d5d1090e6fd
PedroHON410 <pedro.hon200511@gmail.com>
2026-02-15 15:13:19 -0300

    Finalizando a classe venda e atualizando o GerenciamentoEstoque

--- scripts/GerenciamentoEstoque.py
+++ scripts/GerenciamentoEstoque.py
@@ -1,16 +1,21 @@
+from db_gerenciamento import close_connection, create_connection
+
 class GerenciamentoEstoque:
     def __init__(self, qtd_reposicao, id_produto_estoque):
         self.qtd_reposicao = qtd_reposicao
         self.id_produto_estoque = id_produto_estoque
         
+        
     def atualizar_estoque (self):
-        self.id_produto_estoque = int(input('ID do produto para atualizar o estoque: '))
-        if self.id_produto_estoque in Produto.produtos_cadastrados:
-            qtd_reposicao = int(input('Quantidade para reposição: '))
-            if qtd_reposicao > 0:
-                Produto.qtd_estoque += qtd_reposicao
-                return Produto.qtd_estoque
-            else:
-                return 'Quantidade inválida para reposição'
-        else:
-            return 'Produto não encontrado no estoque'
+        connection = create_connection()
+        cursor = connection.cursor()
+        
+        try:
+            update_query = "UPDATE produtos SET qtd_estoque = qtd_estoque + %s WHERE id_produto = %s"
+            cursor.execute(update_query, (self.qtd_reposicao, self.id_produto_estoque))
+            connection.commit()
+            print(f"Estoque do produto ID {self.id_produto_estoque} atualizado! Quantidade adicionada: {self.qtd_reposicao}")
+        except Exception as e:
+            print(f"Erro ao atualizar estoque: {e}")
+        finally:
+            close_connection(connection)
