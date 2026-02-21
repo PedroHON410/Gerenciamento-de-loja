b59b397eab5451f04c8b3a9f76ae712c57ddbd00
PedroHON410 <pedro.hon200511@gmail.com>
2026-02-20 21:04:06 -0300

    Adicionando botoes

--- interface/page1.py
+++ interface/page1.py
@@ -1,17 +1,20 @@
-from customtkinter import *
-
-
-
-class Page1(CTkFrame):
+import customtkinter as ctk 
+class Page1(ctk.CTk):
     def __init__(self, master):
         super().__init__(master)
-        
-        
-        self.label = CTkLabel(self, text="This is page 1")
+        self.title("Sistema de Controle de Loja")
+        self.geometry("1000x700")
+        self.label = ctk.CTkLabel(self, text="Bem-vindo ao Sistema de Controle de Loja!", font=ctk.CTkFont(size=20, weight="bold"))
         self.label.pack(pady=20)
-
-        self.button = CTkButton(self, text="Go to page 2", command=self.go_to_page2)
-        self.button.pack(pady=10)
-
-    def go_to_page2(self):
-        self.master.show_page("page2")
+        self.button1 = ctk.CTkButton(self, text="Gerenciar Produtos",corner_radius=35, fg_color="#C850C0",hover_color="#4158D0",
+                                     command=self.gerenciar_produtos)
+        self.button1.pack(pady=10)
+        self.button2 = ctk.CTkButton(self, text="Gerenciar Vendas",corner_radius=35, fg_color="#C850C0",hover_color="#4158D0")# command=self.gerenciar_vendas)
+        self.button2.pack(pady=10)
+        self.button3 = ctk.CTkButton(self, text="Gerenciar Clientes",corner_radius=35, fg_color="#C850C0",hover_color="#4158D0")#, command=self.gerenciar_clientes)
+        self.button3.pack(pady=10)
+    def gerenciar_produtos(self):
+        print("Gerenciando Produtos...")
+if __name__ == "__main__":
+    app = Page1(None)
+    app.mainloop()
