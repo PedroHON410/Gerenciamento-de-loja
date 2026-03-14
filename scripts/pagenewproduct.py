41897065eb52342a59256a4ae8119b25a8f8729b
PedroHON410 <pedro.hon200511@gmail.com>
2026-03-14 19:51:41 -0300

    Adicionando funcionalidades no pagenewproduct

--- scripts/pagenewproduct.py
+++ scripts/pagenewproduct.py
@@ -8,9 +8,8 @@ class PageNewProduct(ctk.CTk):
         super().__init__()
 
         self.title("Adicionar Novo Produto")
-        self.geometry("400x400")
+        self.geometry("800x600")
         # Configuração de Cores
-        self.cor_roxo = "#740E6D" 
         self.cor_roxo_escuro = "#740E6D"
 
         # Tema e Aparência
@@ -20,17 +19,25 @@ class PageNewProduct(ctk.CTk):
         self.criar_widgets()
     # Widgets
     def criar_widgets(self):
-        # self.main_frame = ctk.CTkFrame(self, fg_color="white", corner_radius=0)
-        # self.main_frame.grid(row=0, column=1, sticky="nsew", padx=20, pady=20)
+        self.sidebar = ctk.CTkFrame(self, width=200, corner_radius=0, fg_color=self.cor_roxo_escuro)
+        self.sidebar.grid(row=0, column=0, sticky="nsew")
+        self.search_entry = ctk.CTkEntry(self.sidebar, placeholder_text="Buscar Produto...", width=100)
+        self.search_entry.pack(pady=20)
         
-    
-        # self.cards_frame = ctk.CTkFrame(self.main_frame, fg_color="transparent")
-        # self.cards_frame.pack(fill="x", pady=10)
-        self.label_titulo = ctk.CTkLabel(self, text="Adicionar Novo Produto", 
-                                        font=ctk.CTkFont(size=20, weight="bold"), text_color="black")
-        
-        self.label_titulo.pack(pady=20)
-        self.entryNome = ctk.CTkEntry(self, placeholder_text="Nome do Produto", width=400)
+        self.label_titulo.pack(pady=0)
+        self.entryNome = ctk.CTkEntry(self, placeholder_text="Nome do Produto", width=300)
         self.entry_preco = ctk.CTkEntry(self, width=300, placeholder_text="Preço")
         self.entry_quantidade = ctk.CTkEntry(self, width=300, placeholder_text="Quantidade")
+    
+
+    def criar_card(self, master, titulo, valor):
+        card = ctk.CTkFrame(master, fg_color=self.cor_roxo_escuro, width=200, corner_radius=10)
+        card.pack(side="top", padx=10)
+        card.pack_propagate(False)
         
+        lbl_titulo = ctk.CTkLabel(card, text=titulo, text_color="white", font=("Arial", 12))
+        lbl_titulo.pack(pady=(10, 0))
+        
+        lbl_valor = ctk.CTkLabel(card, text=valor, text_color="white", font=("Arial", 18, "bold"))
+        lbl_valor.pack()
+        return card
